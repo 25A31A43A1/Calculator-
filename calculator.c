84b18991634bacc55319ddb54abc8ad1e6ab2846
Venkata surya <venkatasurya9347626599@gmail.com>
2026-06-06 10:34:40 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int choice;
+    double num1, num2, result;
+
+    do
+    {
+        printf("\n===== CALCULATOR =====\n");
+        printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("5. Modulus\n");
+        printf("6. Power\n");
+        printf("7. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if(choice >= 1 && choice <= 6)
+        {
+            printf("Enter first number: ");
+            scanf("%lf", &num1);
+
+            printf("Enter second number: ");
+            scanf("%lf", &num2);
+        }
+
+        switch(choice)
+        {
+            case 1:
+                result = num1 + num2;
+                printf("Result = %.2lf\n", result);
+                break;
+            case 2:
+                result = num1 - num2;
+                printf("Result = %.2lf\n", result);
+                break;
+            case 3:
+                result = num1 * num2;
+                printf("Result = %.2lf\n", result);
+                break;
+            case 4:
+                if(num2 != 0)
+                    printf("Result = %.2lf\n", num1 / num2);
+                else
+                    printf("Division by zero not allowed\n");
+                break;
+            case 5:
+                printf("Result = %d\n", (int)num1 % (int)num2);
+                break;
+            case 6:
+                printf("Result = %.2lf\n", pow(num1, num2));
+                break;
+            case 7:
+                printf("Exiting Calculator...\n");
+                break;
+            default:
+                printf("Invalid Choice\n");
+        }
+    } while(choice != 7);
+
+    return 0;
+}
